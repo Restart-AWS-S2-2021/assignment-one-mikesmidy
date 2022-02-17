1d0a8853b732d677ccc4a046c8c2a16c630addc0
mike <mikesmidysmith@gmail.com>
2022-02-16 17:56:42 -0800

    Second to last commit before completing the assessment

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!What goes here?
 
 # Task 5a. This script needs to update Python and install the libraries required to run the server.
+a)
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install python3
+sudo apt install uvicorn
+sudo apt install python3-pip
+pip install fastapi
--- runme.sh
+++ runme.sh
@@ -1,3 +1,6 @@
 #!What goes here?
 
 # This script needs to run uvicorn with the correct IP and port number (last 5 digits of your student ID number)
+5b)
+  #!/bin/bash
+uvicorn main:my_querie_app --host 0.0.0.0:64696
